869b8b619262f0dfcc60bd21131236658af66017
moujahed karim <your.moujahedk83@gmail.com>
2020-07-24 11:08:02 +0200

    les variable

--- fichiertest2.sh
+++ fichiertest2.sh
@@ -1,22 +1,24 @@
-BIRTHDATE =  "Jan 1, 2000"
-Presente = 10
-BIRTHDAY =$( date -d "$BIRTHDATE" +%A )
+#!/bin/bash 
+# les variable
+BIRTHDATE="Jan 1, 2000"
+PREsents=10
+BIRTHDATE=$( date -d "$BIRTHDATE" +%A )
 
 
 # Testing code - do not change it
 
-if [ "$BIRTHDATE" == "Jan 1, 2000" ] ; then
+if [ "BIRTHDATE" == "Jan 1,2000" ] ; then
     echo "BIRTHDATE is correct, it is $BIRTHDATE"
-else
-    echo "BIRTHDATE is incorrect - please retry" 
+ else
+    echo "BIRTHDATE is incorrect - please retry"
 fi
 if [ $Presents == 10 ] ; then
-    echo "I have received $Presents presents"
+    echo "I Have received $Presents presents"
 else
-    echo"Presents is incorrect - please retry"
+    echo"Presents is incorrect - pleise retry" 
 fi
-if [ "$BIRTHDAY" == "Saturday" ] ; then
-    echo "I was born on a $BIRTHDAY"
+if [ "Birthday" == "Sathurday" ] ; then
+   echo " I was born on a $BIRTHDAY"
 else
-    echo "BIRTHDAY" is incorrect - please retry"
-fi         
+   echo "BIRTHDAY is incorrect - please retry"
+fi                 
